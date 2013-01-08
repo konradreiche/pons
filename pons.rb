469261863e2e1c9534f2d5d6b985d6044d0ffb60
Konrad Reiche <konrad.reiche@gmail.com>
2013-01-09 00:21:08 +0100

    Implement functionality
    The whole functionality of this tool is encapsulated in the Pons class
    and accessible over the classes' static methods. Basically, the input
    string is parsed to an array of adb shell commands and then executed one
    after in another in a subshell.
    
    It might be faster to concatenate all adb shell commands with a
    semicolon, but there is an upper limit on the service input length.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

---
+++ pons.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby -w
+
+# Encapsulates the tool functionality: translating a string input to
+# multiple commands and executing the commands on a subshell.
+#
+class Pons
+  @@keycodes = Hash[
+    "#"  => "KEYCODE_POUND",
+    " "  => "KEYCODE_SPACE",
+    "("  => "KEYCODE_LEFT_BRACKET",
+    ")"  => "KEYCODE_RIGHT_BRACKET",
+    "\\" => "KEYCODE_BACKSLASH",
+    ";"  => "KEYCODE_SEMICOLON",
+    "'"  => "KEYCODE_APOSTROPHE"
+  ]
+
+  def self.translate_string_commands(input)
+    input.split(/([#{Regexp.escape @@keycodes.keys.join}])/).map do |str|
+      @@keycodes.key?(str) ?
+      "input keyevent #{@@keycodes[str]}" : "input text #{str}" unless str.empty?
+    end.compact
+  end
+
+  def self.execute_adb_shell_commands(commands)
+    commands.each{|c| system("adb shell #{c}")}
+  end
+end
+
+if __FILE__ == $0
+  commands = Pons.translate_string_commands(ARGV[0])
+  Pons.execute_adb_shell_commands(commands)
+end
